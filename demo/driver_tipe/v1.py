9ad5953bcc64efdd0b417708fc1e0fa30597fa35
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-05 01:42:42 -0400

    Initial commit (clean history)

---
+++ demo/driver_tipe/v1.py
@@ -0,0 +1,20 @@
+# cmd : > pip install pyautogui
+
+import pyautogui
+import time
+
+def auto_type(text, delay=0.5):
+    print("Typing will start in 5 seconds... Move your cursor to a text field.")
+    time.sleep(5)  # Give time to focus a text field
+    for char in text:
+        pyautogui.write(char)
+        time.sleep(delay)
+
+
+# 
+if __name__ == "__main__":
+    sample_text = "This is an automated typing example."
+    auto_type(sample_text, delay=0.5)
+
+
+# This is an automated typing example.
